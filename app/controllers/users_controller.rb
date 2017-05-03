56d38a78d864869165f7302b05072f630fac2189
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-05-04 07:42:20 +0900

    Implement template async CRUD of scaffold user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,48 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:update, :destroy]
 
-  # GET /users
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      respond_to { |f| f.js }
     else
-      render :new
+      respond_to { |f| f.js {
+        render template: 'users/create_error'
+      } }
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      respond_to { |f| f.js }
     else
-      render :edit
+      respond_to { |f| f.js {
+        render template: 'users/update_error'
+      } }
     end
   end
 
-  # DELETE /users/1
   def destroy
-    @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    if @user.destroy
+      respond_to { |f| f.js }
+    else
+      respond_to { |f| f.js {
+        render template: 'users/destroy_error'
+      } }
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:name, :age)
-    end
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
 end
